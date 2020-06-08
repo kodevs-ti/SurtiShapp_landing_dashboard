b7c3b90972a2034dad96e7e9555a72eb59e4a7b4
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-06-08 14:15:25 -0500

    [UPDATE] Project Base

---
+++ components/Layout/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Layout'
+
+export default function Landing () {
+  return (
+    <Layout>
+      <div className='container-fluid'>
+        <h2>Welcome to SurtiShapp</h2>
+      </div>
+    </Layout>
+  )
+}
