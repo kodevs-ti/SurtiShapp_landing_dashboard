f8d66fc662069a07a28452b7f256f141722ed020
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-06-08 14:55:34 -0500

    [UPDATE] Project Base

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/app.scss'
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default function MyApp ({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
